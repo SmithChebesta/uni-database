a57e23f319fd77e95f952e628dbebe59afc41fbc
SmithChebesta <kaimot2010@hotmail.co.th>
2019-11-15 08:44:53 +0700

    update model

--- display/models.py
+++ display/models.py
@@ -5,18 +5,18 @@ import datetime
 
 class Tag(models.Model):
     tag = models.TextField(primary_key=True)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, default=None)
 
 
 class Customer(models.Model):
     customer_name = models.TextField()
     customer_id = models.TextField(primary_key=True)
-    accounting_name = models.TextField(blank=True, default='-')
-    accounting_contact = models.TextField(blank=True, default='-')
-    technical_name = models.TextField(blank=True, default='-')
-    technical_contact = models.TextField(blank=True, default='-')
+    accounting_name = models.TextField(blank=True, default=None, null=True)
+    accounting_contact = models.TextField(blank=True, default=None, null=True)
+    technical_name = models.TextField(blank=True, default=None, null=True)
+    technical_contact = models.TextField(blank=True, default=None, null=True)
     tag = models.ForeignKey("Tag", on_delete=models.CASCADE)
-    distributor_name = models.TextField(blank=True, default='-')
+    distributor_name = models.TextField(blank=True, default=None, null=True)
 
 
 class Webapp (models.Model):
@@ -26,7 +26,8 @@ class Webapp (models.Model):
     super_admin_id = models.TextField()
     super_admin_password = models.TextField()
     url = models.URLField(max_length=200)
-    drive = models.URLField(max_length=200, blank=True, default='-')
+    drive = models.URLField(max_length=200, blank=True,
+                            default=None, null=True)
     max_users = models.IntegerField()
     organizationID = models.TextField()
     status = models.BooleanField(default=False)
@@ -35,7 +36,7 @@ class Webapp (models.Model):
 class service(models.Model):
     customer_id = models.ForeignKey("Customer", on_delete=models.CASCADE)
     system_name = models.OneToOneField("Webapp", on_delete=models.CASCADE)
-    SKU = models.TextField(default='Some generated text')
+    # SKU = models.TextField(default='Some generated text')
     service_start_date = models.DateField()
     service_end_date = models.DateField()
     product_type = models.TextField()
@@ -54,11 +55,11 @@ class service(models.Model):
 class Gateway(models.Model):
     gateway_id = models.TextField(primary_key=True)
     customer_id = models.ForeignKey("Customer", on_delete=models.CASCADE)
-
     refresh_rate = models.FloatField(blank=True, default=0)
-    uid_list = models.TextField(blank=True, default='-')
-    system = models.TextField(blank=True, default='-')
-    IMEI_MAC = models.TextField(blank=True, default='-')
-    firmware_upgrade = models.TextField(blank=True, default='-')
-    moblie_number = models.TextField(blank=True, default='-')
+    uid_list = models.TextField()
+    system_name = models.ForeignKey("service", on_delete=models.CASCADE)
+    # system = models.TextField(blank=True, default=None, null=True)
+    IMEI_MAC = models.TextField()
+    firmware_upgrade = models.TextField(blank=True, default=None, null=True)
+    moblie_number = models.TextField(blank=True, default=None, null=True)
     max_sms = models.IntegerField(blank=True, default=0)
