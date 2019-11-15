cd9fe3bda774c377e0718311e8aca9eac7631b4a
SmithChebesta <kaimot2010@hotmail.co.th>
2019-11-15 08:54:26 +0700

    Update settings.py

--- uni_database/settings.py
+++ uni_database/settings.py
@@ -126,5 +126,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
