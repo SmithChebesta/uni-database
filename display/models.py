6811c49e5d531209e0e7826efbb28f94481cbfbd
SmithChebesta <kaimot2010@hotmail.co.th>
2019-11-16 12:38:40 +0700

    hehexd

--- display/models.py
+++ display/models.py
@@ -5,18 +5,18 @@ import datetime
 
 class Tag(models.Model):
     tag = models.TextField(primary_key=True)
-    description = models.TextField(null=True, default=None)
+    description = models.TextField(null=True,blank=True, default=None)
 
 
 class Customer(models.Model):
     customer_name = models.TextField()
     customer_id = models.TextField(primary_key=True)
+    tag = models.ForeignKey("Tag", on_delete=models.CASCADE)
+    distributor_name = models.TextField(blank=True, default=None, null=True)
     accounting_name = models.TextField(blank=True, default=None, null=True)
     accounting_contact = models.TextField(blank=True, default=None, null=True)
     technical_name = models.TextField(blank=True, default=None, null=True)
     technical_contact = models.TextField(blank=True, default=None, null=True)
-    tag = models.ForeignKey("Tag", on_delete=models.CASCADE)
-    distributor_name = models.TextField(blank=True, default=None, null=True)
 
 
 class Webapp (models.Model):
@@ -35,31 +35,30 @@ class Webapp (models.Model):
 
 class service(models.Model):
     customer_id = models.ForeignKey("Customer", on_delete=models.CASCADE)
-    system_name = models.OneToOneField("Webapp", on_delete=models.CASCADE)
-    # SKU = models.TextField(default='Some generated text')
+    system_name = models.OneToOneField(
+        "Webapp", on_delete=models.CASCADE, null=True, blank=True)
     service_start_date = models.DateField()
     service_end_date = models.DateField()
     product_type = models.TextField()
     service_type = models.TextField()
-
     @property
     def status(self):
         now = datetime.date.today()
         return self.service_start_date < now and now < self.service_end_date
 
     @property
-    def calculate_duration(self):
+    def duration(self):
         return f'{(self.service_end_date - self.service_start_date).days} days'
 
 
 class Gateway(models.Model):
     gateway_id = models.TextField(primary_key=True)
     customer_id = models.ForeignKey("Customer", on_delete=models.CASCADE)
-    refresh_rate = models.FloatField(blank=True, default=0)
+    system_name = models.ForeignKey(
+        "service", on_delete=models.CASCADE, blank=True, null=True)
     uid_list = models.TextField()
-    system_name = models.ForeignKey("service", on_delete=models.CASCADE)
-    # system = models.TextField(blank=True, default=None, null=True)
     IMEI_MAC = models.TextField()
+    refresh_rate = models.FloatField()
     firmware_upgrade = models.TextField(blank=True, default=None, null=True)
     moblie_number = models.TextField(blank=True, default=None, null=True)
-    max_sms = models.IntegerField(blank=True, default=0)
+    max_sms = models.IntegerField(blank=True, default=0, null=True)
